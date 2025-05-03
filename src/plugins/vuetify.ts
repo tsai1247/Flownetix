081316435b4fb13c51eddde0e5b262204e484110
tsai1247 <jeff29cc91079@gmail.com>
2025-05-03 22:04:15 +0800

    feat: set vuetify light theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -14,6 +14,19 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1A365D', // 定義 primary 顏色
+          secondary: '#FFFFFF',
+          background: '#F8FAFC',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+        },
+      },
+    },
   },
 })
