2d346cb73992cb5e6199450a6727954ef1443563
tsai1247 <jeff29cc91079@gmail.com>
2025-05-04 23:57:36 +0800

    feat: add AssetsDB for managing asset records in IndexedDB

---
+++ src/common/indexedDB/AssetsDB.ts
@@ -0,0 +1,35 @@
+import type { BaseDBDataType } from './IDatabase';
+import BaseDB from './BaseDB';
+
+interface Asset extends BaseDBDataType {
+  name: string;
+  type: string;
+  institution: string;
+  balance: number;
+  currency: string;
+  website: string;
+}
+
+class AssetsDB extends BaseDB<Asset> {
+  constructor () {
+    const dbName = 'nexus-finance';
+    const tableName = 'assets';
+    const pk = 'id';
+    const columns = [
+      { key: 'name', unique: false },
+      { key: 'type', unique: false },
+      { key: 'institution', unique: false },
+      { key: 'balance', unique: false },
+      { key: 'currency', unique: false },
+      { key: 'website', unique: false },
+      { key: 'cons_date', unique: false },
+      { key: 'modi_date', unique: false },
+    ];
+    super(dbName, tableName, pk, columns);
+  }
+}
+
+const assetsDB = new AssetsDB();
+
+export default assetsDB;
+export type { Asset };
---
+++ src/common/indexedDB/index.ts
@@ -0,0 +1,5 @@
+import assetsDB from './AssetsDB';
+import type { Asset } from './AssetsDB';
+
+export { assetsDB };
+export type { Asset };
