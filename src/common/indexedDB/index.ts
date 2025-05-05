560a73ed06e583cb1eab918ab7c1867ad147e2b7
tsai1247 <jeff29cc91079@gmail.com>
2025-05-05 23:09:05 +0800

    feat: add InstitutionDB for managing institution data in IndexedDB

--- src/common/indexedDB/index.ts
+++ src/common/indexedDB/index.ts
@@ -1,5 +1,10 @@
 import assetsDB from './AssetsDB';
 import type { Asset } from './AssetsDB';
 
+import institutionDB from './InstitutionDB';
+import type { Institution } from './InstitutionDB';
+
 export { assetsDB };
 export type { Asset };
+export { institutionDB };
+export type { Institution };
