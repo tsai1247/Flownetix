1143c9f94c5751395bebd2773d590a6b83194fb5
tsai1247 <jeff29cc91079@gmail.com>
2025-05-15 01:34:32 +0800

    feat: implement getOneYearList to aggregate cash flow data for the upcoming year

--- src/api/cashFlow/CashFlowApi.ts
+++ src/api/cashFlow/CashFlowApi.ts
@@ -1,5 +1,7 @@
 import { cashFlowDB } from '@/common/indexedDB';
 import type { CashFlow } from '@/dataType';
+import { FlowType, Interval } from '@/dataType';
+import { useLocale } from 'vuetify';
 
 function getAll (): Promise<CashFlow[]> {
   return new Promise((resolve, reject) => {
@@ -58,9 +60,89 @@ function remove (cashFlow: CashFlow) {
   });
 }
 
+async function getOneYearList () {
+  const chartData = [];
+  const { current } = useLocale();
+
+  const addData = cashFlow => {
+    const yearId = new Date(cashFlow.startDate).getFullYear();
+    const monthId = new Date(cashFlow.startDate).getMonth();
+    const targetIndex = chartData.findIndex(item => item.yearId === yearId && item.monthId === monthId);
+    if (targetIndex === -1) return false;
+    if(cashFlow.flowType === FlowType.Income) {
+      chartData[targetIndex] = {
+        ...chartData[targetIndex],
+        value: chartData[targetIndex].value + parseInt(cashFlow.amount, 10),
+        income: chartData[targetIndex].income + parseInt(cashFlow.amount, 10),
+      };
+    }
+    else {
+      chartData[targetIndex] = {
+        ...chartData[targetIndex],
+        value: chartData[targetIndex].value - parseInt(cashFlow.amount, 10),
+        expenses: chartData[targetIndex].expenses + parseInt(cashFlow.amount, 10),
+      };
+    }
+    return true;
+  }
+
+  for (let i = 0; i < 12; i++) {
+    const targetDate = new Date();
+    targetDate.setMonth(targetDate.getMonth() + i)
+    targetDate.setDate(1)
+    const month = targetDate.toLocaleString(current.value, { month: 'short' });
+    const yearId = targetDate.getFullYear();
+    const monthId = targetDate.getMonth();
+    chartData.push({
+      yearId,
+      monthId,
+      xAxis: month,
+      value: 0,
+      income: 0,
+      expenses: 0,
+    });
+  }
+
+  await getAll().then(data => {
+    data.forEach(cashFlow => {
+      if(cashFlow.isRecurring) {
+        while(true){
+          // just add to target month
+          if(!addData(cashFlow)){
+            break;
+          }
+
+          const newDate = new Date(cashFlow.startDate);
+          switch (cashFlow.recurringRate?.unit) {
+            case Interval.Day:
+              newDate.setDate(newDate.getDate() + cashFlow.recurringRate.per);
+              break;
+            case Interval.Month:
+              newDate.setMonth(newDate.getMonth() + cashFlow.recurringRate.per);
+              break;
+            case Interval.Quarter:
+              newDate.setMonth(newDate.getMonth() + cashFlow.recurringRate.per * 3);
+              break;
+            case Interval.Year:
+              newDate.setFullYear(newDate.getFullYear() + cashFlow.recurringRate.per);
+              break;
+          }
+
+          cashFlow.startDate = newDate;
+        }
+      }
+      else {
+        addData(cashFlow);
+      }
+    })
+  })
+  return chartData;
+}
+
 export default {
   getAll,
   add,
   update,
   remove,
+  getOneYearList,
 };
--- src/components/cashFlow/cahFlowManager/cashFlowProjection.vue
+++ src/components/cashFlow/cahFlowManager/cashFlowProjection.vue
@@ -17,98 +17,28 @@
 
 <script setup lang="ts">
   import VChart from 'vue-echarts';
-  import { computed, onMounted, reactive } from 'vue';
+  import { computed, onMounted, ref } from 'vue';
   import apis from '../../../api';
-  import { useLocale } from 'vuetify';
-  import { FlowType, Interval } from '@/dataType';
-  const { current } = useLocale();
 
-  const chartData = reactive([]);
+  const chartData = ref([]);
   const progressiveChartData = computed(() => {
     const result = [];
-    for(let i=0; i<chartData.length; i++) {
+    for(let i=0; i<chartData.value.length; i++) {
       const lastValue = i == 0 ? 0 : result[i-1].value;
 
       result.push({
-        ...chartData[i],
-        value: chartData[i].value + lastValue,
+        ...chartData.value[i],
+        value: chartData.value[i].value + lastValue,
       })
     }
     return result;
   });
 
   onMounted(() => {
-    const now = new Date();
-    for (let i = 0; i < 12; i++) {
-      const targetDate = new Date();
-      targetDate.setMonth(targetDate.getMonth() + i)
-      targetDate.setDate(1)
-      const month = targetDate.toLocaleString(current.value, { month: 'short' });
-      const yearId = targetDate.getFullYear();
-      const monthId = targetDate.getMonth();
-      chartData.push({
-        yearId,
-        monthId,
-        xAxis: month,
-        value: 0,
-      });
-    }
-
-    apis.cashFlow.getAll().then(data => {
-      data.forEach(cashFlow => {
-        console.log(cashFlow);
-        if(cashFlow.isRecurring) {
-          while(true){
-            // just add to target month
-            if(!addData(cashFlow)){
-              break;
-            }
-
-            const newDate = new Date(cashFlow.startDate);
-            switch (cashFlow.recurringRate?.unit) {
-              case Interval.Day:
-                newDate.setDate(newDate.getDate() + cashFlow.recurringRate.per);
-                break;
-              case Interval.Month:
-                newDate.setMonth(newDate.getMonth() + cashFlow.recurringRate.per);
-                break;
-              case Interval.Quarter:
-                newDate.setMonth(newDate.getMonth() + cashFlow.recurringRate.per * 3);
-                break;
-              case Interval.Year:
-                newDate.setFullYear(newDate.getFullYear() + cashFlow.recurringRate.per);
-                break;
-            }
-
-            cashFlow.startDate = newDate;
-          }
-        }
-        else {
-          addData(cashFlow);
-        }
-      })
-    })
+    apis.cashFlow.getOneYearList().then(yearList => {
+      chartData.value = yearList;
+    });
   });
-
-  const addData = cashFlow => {
-    const yearId = new Date(cashFlow.startDate).getFullYear();
-    const monthId = new Date(cashFlow.startDate).getMonth();
-    const targetIndex = chartData.findIndex(item => item.yearId === yearId && item.monthId === monthId);
-    if (targetIndex === -1) return false;
-    if(cashFlow.flowType === FlowType.Income) {
-      chartData[targetIndex] = {
-        ...chartData[targetIndex],
-        value: chartData[targetIndex].value + parseInt(cashFlow.amount, 10),
-      };
-    }
-    else {
-      chartData[targetIndex] = {
-        ...chartData[targetIndex],
-        value: chartData[targetIndex].value - parseInt(cashFlow.amount, 10),
-      };
-    }
-    return true;
-  }
 
   const option = computed(() => {
     return {
